3327c3cb2c956ead1735c55181f034a24f478d62
Pedro Arthur <pedro13arthur@gmail.com>
2023-03-11 17:04:00 -0300

    ui kitten

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import * as eva from "@eva-design/eva";
+import {
+  ApplicationProvider,
+  IconRegistry,
+  Layout,
+  Text,
+  Radio,
+  Icon,
+} from "@ui-kitten/components";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
+import { StyleSheet } from "react-native";
 
-export default function App() {
+const HomeScreen = () => {
+  const [checked, setChecked] = React.useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Layout style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text category="h1">HOME</Text>
+      <Radio
+        checked={checked}
+        onChange={(nextChecked) => setChecked(nextChecked)}
+      >
+        {`Checked: ${checked}`}
+      </Radio>
+      <Icon style={styles.icon} fill="#8F9BB3" name="star" />
+    </Layout>
   );
-}
+};
+
+export default () => (
+  <>
+    <IconRegistry icons={EvaIconsPack} />
+    <ApplicationProvider {...eva} theme={eva.dark}>
+      <HomeScreen />
+    </ApplicationProvider>
+  </>
+);
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  icon: {
+    width: 32,
+    height: 32,
   },
 });
