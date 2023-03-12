be3e19a20a7211b032e2b72b823d794f48bc420b
Pedro Arthur <pedro13arthur@gmail.com>
2023-03-12 16:15:27 -0300

    theme

--- App.js
+++ App.js
@@ -7,9 +7,22 @@ import {
   Text,
   Radio,
   Icon,
+  Button,
+  Spinner,
 } from "@ui-kitten/components";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { default as theme } from "./src/theme/theme.json";
+
+const StarIcon = (props) => (
+  <Icon {...props} style={styles.icon} fill="white" name="star" />
+);
+
+const LoadingIndicator = (props) => (
+  <View style={[props.style, styles.indicator]}>
+    <Spinner size="small" />
+  </View>
+);
 
 const HomeScreen = () => {
   const [checked, setChecked] = React.useState(false);
@@ -23,7 +36,31 @@ const HomeScreen = () => {
       >
         {`Checked: ${checked}`}
       </Radio>
-      <Icon style={styles.icon} fill="#8F9BB3" name="star" />
+      <Icon style={styles.icon} fill="#8F9BB3" name="facebook" />
+      <Button style={styles.button} status="primary" accessoryLeft={StarIcon}>
+        PRIMARY
+      </Button>
+
+      <Button style={styles.button} status="success" accessoryRight={StarIcon}>
+        SUCCESS
+      </Button>
+
+      <Button style={styles.button} status="danger" accessoryLeft={StarIcon} />
+
+      <Button
+        style={styles.button}
+        appearance="ghost"
+        status="danger"
+        accessoryLeft={StarIcon}
+      />
+
+      <Button
+        style={styles.button}
+        appearance="outline"
+        accessoryLeft={LoadingIndicator}
+      >
+        LOADING
+      </Button>
     </Layout>
   );
 };
@@ -31,7 +68,7 @@ const HomeScreen = () => {
 export default () => (
   <>
     <IconRegistry icons={EvaIconsPack} />
-    <ApplicationProvider {...eva} theme={eva.dark}>
+    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
       <HomeScreen />
     </ApplicationProvider>
   </>
@@ -41,5 +78,16 @@ const styles = StyleSheet.create({
   icon: {
     width: 32,
     height: 32,
+  },
+  container: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  button: {
+    margin: 2,
+  },
+  indicator: {
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
