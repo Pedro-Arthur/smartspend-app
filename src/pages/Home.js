86244d6f7cb413c2f46b84ebc4c64d3f710d548a
Pedro Arthur <pedro13arthur@gmail.com>
2023-07-06 20:07:56 -0300

    toggle theme

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { useColorModeValue, Heading, Center, Button, Switch } from 'native-base';
+import {
+  useColorModeValue,
+  Heading,
+  Center,
+  Button,
+  Switch,
+  useColorMode,
+  Text,
+  HStack,
+} from 'native-base';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { CommonActions } from '@react-navigation/native';
 
 const Home = ({ navigation }) => {
   const bg = useColorModeValue('warmGray.50', 'coolGray.800');
+  const { colorMode, toggleColorMode } = useColorMode();
 
   const logout = async () => {
     await AsyncStorage.removeItem('@user');
@@ -20,7 +30,14 @@ const Home = ({ navigation }) => {
   return (
     <Center flex="1" bg={bg} safeArea>
       <Heading>HOME</Heading>
-      <Switch mt={5} size="lg" />
+
+      <Text my={5}>Tema atual: {colorMode}</Text>
+
+      <HStack flexShrink={1} alignItems="center">
+        <Text>Modo escuro:</Text>
+        <Switch isChecked={colorMode === 'dark'} onToggle={toggleColorMode} mt={5} size="lg" />
+      </HStack>
+
       <Button mt={5} onPress={logout} w={40} backgroundColor="red.500">
         Sair
       </Button>
