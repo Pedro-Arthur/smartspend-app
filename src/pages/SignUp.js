5622afe829fa79bb45f4e822d80025383a4b040e
Pedro Arthur <pedro13arthur@gmail.com>
2023-07-07 12:08:51 -0300

    fix

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -10,8 +10,10 @@ import {
   FormControl,
   Input,
   IconButton,
+  Icon,
+  Pressable,
 } from 'native-base';
-import { Feather } from '@expo/vector-icons';
+import { Feather, AntDesign } from '@expo/vector-icons';
 import { ToastContext } from '../contexts/ToastContext';
 import GoogleLogo from '../assets/images/google-logo.svg';
 
@@ -31,6 +33,7 @@ const SignUp = ({ navigation }) => {
     password: null,
     confirmPassword: null,
   });
+  const [showPassword, setShowPassword] = useState(false);
 
   const validate = () => {
     const errors = { name: null, email: null, password: null, confirmPassword: null };
@@ -96,6 +99,9 @@ const SignUp = ({ navigation }) => {
           <FormControl isRequired isInvalid={formErrors.name}>
             <FormControl.Label>Nome</FormControl.Label>
             <Input
+              InputLeftElement={
+                <Icon as={<AntDesign name="user" />} size={4} ml="3" color="muted.400" />
+              }
               placeholder="João Silva"
               onChangeText={(value) => setFormData({ ...formData, name: value })}
             />
@@ -107,6 +113,9 @@ const SignUp = ({ navigation }) => {
           <FormControl isRequired isInvalid={formErrors.email}>
             <FormControl.Label>E-mail</FormControl.Label>
             <Input
+              InputLeftElement={
+                <Icon as={<AntDesign name="mail" />} size={4} ml="3" color="muted.400" />
+              }
               keyboardType="email-address"
               placeholder="joao@email.com"
               onChangeText={(value) => setFormData({ ...formData, email: value })}
@@ -119,9 +128,19 @@ const SignUp = ({ navigation }) => {
           <FormControl isRequired isInvalid={formErrors.password}>
             <FormControl.Label>Senha</FormControl.Label>
             <Input
+              type={showPassword ? 'text' : 'password'}
+              InputRightElement={
+                <Pressable onPress={() => setShowPassword(!showPassword)}>
+                  <Icon
+                    as={<Feather name={showPassword ? 'eye' : 'eye-off'} />}
+                    size={4}
+                    mr="3"
+                    color="muted.400"
+                  />
+                </Pressable>
+              }
               placeholder="******"
               onChangeText={(value) => setFormData({ ...formData, password: value })}
-              type="password"
             />
             {'password' in formErrors && (
               <FormControl.ErrorMessage>{formErrors.password}</FormControl.ErrorMessage>
@@ -131,9 +150,19 @@ const SignUp = ({ navigation }) => {
           <FormControl isRequired isInvalid={formErrors.confirmPassword}>
             <FormControl.Label>Confirme a senha</FormControl.Label>
             <Input
+              type={showPassword ? 'text' : 'password'}
+              InputRightElement={
+                <Pressable onPress={() => setShowPassword(!showPassword)}>
+                  <Icon
+                    as={<Feather name={showPassword ? 'eye' : 'eye-off'} />}
+                    size={4}
+                    mr="3"
+                    color="muted.400"
+                  />
+                </Pressable>
+              }
               placeholder="******"
               onChangeText={(value) => setFormData({ ...formData, confirmPassword: value })}
-              type="password"
             />
             {'confirmPassword' in formErrors && (
               <FormControl.ErrorMessage>{formErrors.confirmPassword}</FormControl.ErrorMessage>
