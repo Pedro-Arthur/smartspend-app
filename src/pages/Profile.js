5f3dc9847c0d363ad0f2b1984d6fccd01e92f490
Pedro Arthur <pedro13arthur@gmail.com>
2023-07-07 13:35:08 -0300

    fix

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import { useColorModeValue, Heading, Center } from 'native-base';
+import React, { useContext } from 'react';
+import { useColorModeValue, Heading, Center, Text } from 'native-base';
+import { AuthContext } from '../contexts/AuthContext';
 
 const Profile = () => {
   const bg = useColorModeValue('warmGray.50', 'coolGray.800');
+  const { user } = useContext(AuthContext);
 
   return (
     <Center flex="1" bg={bg} safeArea>
       <Heading>PROFILE</Heading>
+      <Text>{user.name}</Text>
     </Center>
   );
 };
