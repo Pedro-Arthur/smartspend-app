92d9131d290fd026e140ac44a064fe5d26a36e5a
Pedro Arthur <pedro13arthur@gmail.com>
2023-07-13 13:15:15 -0300

    interceptor api

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,24 @@
+import { useContext } from 'react';
 import axios from 'axios';
 import { API_BASE_URL } from '@env';
+import { AuthContext } from '../contexts/AuthContext';
 
 const baseURL = API_BASE_URL;
 
 const api = axios.create({
   baseURL,
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response && error.response.status === 401 && error.config.url !== '/auth/login') {
+      const { removeAuthIsLoggedIn, removeAuthUser, removeAuthToken } = useContext(AuthContext);
+      await Promise.all([removeAuthIsLoggedIn(), removeAuthUser(), removeAuthToken()]);
+    }
+
+    throw error;
+  }
+);
 
 export default api;
