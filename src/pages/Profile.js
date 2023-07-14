842f3beac274e348e91d66e45cd013f2b0884dc0
Pedro Arthur <pedro13arthur@gmail.com>
2023-07-14 20:36:01 -0300

    fix

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,12 +4,14 @@ import { AuthContext } from '../contexts/AuthContext';
 
 const Profile = () => {
   const bg = useColorModeValue('warmGray.50', 'coolGray.800');
-  const { user } = useContext(AuthContext);
+  const { user, isLoggedIn, token } = useContext(AuthContext);
 
   return (
     <Center flex="1" bg={bg} safeArea>
       <Heading>PROFILE</Heading>
       <Text>{user.name}</Text>
+      <Text>{isLoggedIn}</Text>
+      <Text>{token}</Text>
     </Center>
   );
 };
