e6e2eb515267e3177efb042962a34b94f8acf872
Pedro Arthur <pedro13arthur@gmail.com>
2023-07-14 20:16:18 -0300

    remove local auth

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { Image, StyleSheet } from 'react-native';
-import { Button, Center, NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider } from 'native-base';
 import { StatusBar } from 'expo-status-bar';
 import { preventAutoHideAsync } from 'expo-splash-screen';
-import * as LocalAuthentication from 'expo-local-authentication';
 import 'react-native-gesture-handler';
 
 import theme from './src/theme';
@@ -19,31 +17,13 @@ import { AuthProvider } from './src/contexts/AuthContext';
 
 export default () => {
   const [fontLoaded, setFontLoaded] = useState(false);
-  const [authenticated, setAuthenticated] = useState(false);
 
   const callPreventAutoHideAsync = async () => {
     await preventAutoHideAsync();
   };
 
-  const handleAuthenticate = async () => {
-    const available = await LocalAuthentication.hasHardwareAsync();
-
-    if (available) {
-      const result = await LocalAuthentication.authenticateAsync({
-        promptMessage: 'Autenticação obrigatória',
-      });
-
-      if (result.success || result.error === 'not_enrolled') {
-        setAuthenticated(true);
-      }
-    } else {
-      setAuthenticated(true);
-    }
-  };
-
   const handleFinishFontsLoading = useCallback(() => {
     setFontLoaded(true);
-    handleAuthenticate();
   }, []);
 
   useEffect(() => {
@@ -58,34 +38,15 @@ export default () => {
     <NativeBaseProvider theme={theme} colorModeManager={colorModeManager}>
       <StatusBar backgroundColor="#5E17EB" barStyle="light-content" />
 
-      {authenticated ? (
-        <FetchLoadingProvider>
-          <FetchLoading />
+      <FetchLoadingProvider>
+        <FetchLoading />
 
-          <ToastProvider>
-            <AuthProvider>
-              <Main />
-            </AuthProvider>
-          </ToastProvider>
-        </FetchLoadingProvider>
-      ) : (
-        <Center backgroundColor="white" safeArea flex={1}>
-          <Image
-            source={require('./src/assets/images/fingerprint-scan.gif')}
-            style={styles.image}
-          />
-          <Button mt={2} width={200} onPress={handleAuthenticate}>
-            Autenticar
-          </Button>
-        </Center>
-      )}
+        <ToastProvider>
+          <AuthProvider>
+            <Main />
+          </AuthProvider>
+        </ToastProvider>
+      </FetchLoadingProvider>
     </NativeBaseProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  image: {
-    width: 200,
-    height: 200,
-  },
-});
