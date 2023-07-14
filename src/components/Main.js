967921af54d2e579fba1bae4bc8c4a43bbaea03f
Pedro Arthur <pedro13arthur@gmail.com>
2023-07-14 20:07:26 -0300

    fix

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useContext } from 'react';
 import * as NetInfo from '@react-native-community/netinfo';
-
 import Routes from '../routes';
 import { ToastContext } from '../contexts/ToastContext';
+import { AuthContext } from '../contexts/AuthContext';
+import api from '../services/api';
 
 const Main = () => {
   const { showToast } = useContext(ToastContext);
+  const { removeAuthIsLoggedIn } = useContext(AuthContext);
 
   const checkInternetConnection = async () => {
     const netInfo = await NetInfo.fetch();
@@ -24,6 +26,17 @@ const Main = () => {
   useEffect(() => {
     checkInternetConnection();
   }, []);
+
+  api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      if (error.response && error.response.status === 401 && error.config.url !== '/auth/login') {
+        await removeAuthIsLoggedIn();
+      }
+
+      throw error;
+    }
+  );
 
   return <Routes />;
 };
--- src/services/api.js
+++ src/services/api.js
@@ -1,24 +1,10 @@
-import { useContext } from 'react';
 import axios from 'axios';
 import { API_BASE_URL } from '@env';
-import { AuthContext } from '../contexts/AuthContext';
 
 const baseURL = API_BASE_URL;
 
 const api = axios.create({
   baseURL,
 });
-
-api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    if (error.response && error.response.status === 401 && error.config.url !== '/auth/login') {
-      const { removeAuthIsLoggedIn } = useContext(AuthContext);
-      await Promise.all([removeAuthIsLoggedIn()]);
-    }
-
-    throw error;
-  }
-);
 
 export default api;
