c5856fa306f8fb68d5380fad7a5261807c9d4c51
Pedro Arthur <pedro13arthur@gmail.com>
2023-07-16 20:19:06 -0300

    fix

--- src/components/TermsModal.js
+++ src/components/TermsModal.js
@@ -1,13 +1,134 @@
 import React, { useState } from 'react';
-import { Button, Modal, Box, useColorModeValue, Text, ScrollView } from 'native-base';
-import { Animated, Pressable, Dimensions } from 'react-native';
+import { Button, Modal, Box, useColorModeValue, Text, ScrollView, Heading } from 'native-base';
+import { Animated, Dimensions, Pressable } from 'react-native';
 import { TabView, SceneMap } from 'react-native-tab-view';
 
 const screenHeight = Dimensions.get('window').height;
 
 const TermsTab = () => (
   <ScrollView my="3">
-    <Text>frfrfrr</Text>
+    <Heading size="md" my={3}>
+      1. Termos
+    </Heading>
+
+    <Text>
+      Ao acessar ao aplicativo SmartSpend, concorda em cumprir estes termos de serviço, todas as
+      leis e regulamentos aplicáveis e concorda que é responsável pelo cumprimento de todas as leis
+      locais aplicáveis. Se você não concordar com algum desses termos, está proibido de usar ou
+      acessar este aplicativo. Os materiais contidos neste aplicativo são protegidos pelas leis de
+      direitos autorais e marcas comerciais aplicáveis.
+    </Text>
+
+    <Heading size="md" my={3}>
+      2. Uso de Licença
+    </Heading>
+
+    <Text>
+      É concedida permissão para baixar temporariamente uma cópia dos materiais (informações ou
+      software) no aplicativo SmartSpend, apenas para visualização transitória pessoal e não
+      comercial. Esta é a concessão de uma licença, não uma transferência de título e, sob esta
+      licença, você não pode:
+    </Text>
+
+    <Text ml="5">2.1 modificar ou copiar os materiais;</Text>
+
+    <Text ml="5">
+      2.2 usar os materiais para qualquer finalidade comercial ou para exibição pública (comercial
+      ou não comercial);
+    </Text>
+
+    <Text ml="5">
+      2.3 tentar descompilar ou fazer engenharia reversa de qualquer software contido no aplicativo
+      SmartSpend;
+    </Text>
+
+    <Text ml="5">
+      2.4 remover quaisquer direitos autorais ou outras notações de propriedade dos materiais; ou
+    </Text>
+
+    <Text ml="5">
+      2.5 transferir os materiais para outra pessoa ou espelhe os materiais em qualquer outro
+      servidor.
+    </Text>
+
+    <Text>
+      Esta licença será automaticamente rescindida se você violar alguma dessas restrições e poderá
+      ser rescindida por SmartSpend a qualquer momento. Ao encerrar a visualização desses materiais
+      ou após o término desta licença, você deve apagar todos os materiais baixados em sua posse,
+      seja em formato eletrônico ou impresso.
+    </Text>
+
+    <Heading size="md" my={3}>
+      3. Isenção de responsabilidade
+    </Heading>
+
+    <Text>
+      3.1 Os materiais no aplicativo SmartSpend são fornecidos como estão. SmartSpend não oferece
+      garantias, expressas ou implícitas, e, por este meio, isenta e nega todas as outras garantias,
+      incluindo, sem limitação, garantias implícitas ou condições de comercialização, adequação a um
+      fim específico ou não violação de propriedade intelectual ou outra violação de direitos.
+    </Text>
+    <Text>
+      3.2 Além disso, SmartSpend não garante ou faz qualquer representação relativa à precisão, aos
+      resultados prováveis ou à confiabilidade do uso dos materiais em seu aplicativo ou de outra
+      forma relacionado a esses materiais ou em sites vinculados a este aplicativo.
+    </Text>
+
+    <Heading size="md" my={3}>
+      4. Limitações
+    </Heading>
+
+    <Text>
+      Em nenhum caso SmartSpend ou seus fornecedores serão responsáveis por quaisquer danos
+      (incluindo, sem limitação, danos por perda de dados ou lucro ou devido a interrupção dos
+      negócios) decorrentes do uso ou da incapacidade de usar os materiais em SmartSpend, mesmo que
+      SmartSpend ou um representante autorizado da SmartSpend tenha sido notificado oralmente ou por
+      escrito da possibilidade de tais danos. Como algumas jurisdições não permitem limitações em
+      garantias implícitas, ou limitações de responsabilidade por danos consequentes ou incidentais,
+      essas limitações podem não se aplicar a você.
+    </Text>
+
+    <Heading size="md" my={3}>
+      5. Precisão dos materiais
+    </Heading>
+
+    <Text>
+      Os materiais exibidos no aplicativo SmartSpend podem incluir erros técnicos, tipográficos ou
+      fotográficos. SmartSpend não garante que qualquer material em seu aplicativo seja preciso,
+      completo ou atual. SmartSpend pode fazer alterações nos materiais contidos em seu aplicativo a
+      qualquer momento, sem aviso prévio. No entanto, SmartSpend não se compromete a atualizar os
+      materiais.
+    </Text>
+
+    <Heading size="md" my={3}>
+      6. Links
+    </Heading>
+
+    <Text>
+      SmartSpend não analisou todos os sites vinculados ao seu aplicativo e não é responsável pelo
+      conteúdo de nenhum site vinculado. A inclusão de qualquer link não implica endosso por
+      SmartSpend do site. O uso de qualquer site vinculado é por conta e risco do usuário.
+    </Text>
+
+    <Heading size="sm" my={3}>
+      Modificações
+    </Heading>
+
+    <Text>
+      SmartSpend pode revisar estes termos de serviço do aplicativo a qualquer momento, sem aviso
+      prévio. Ao usar este aplicativo, você concorda em ficar vinculado à versão atual desses termos
+      de serviço.
+    </Text>
+
+    <Heading size="sm" my={3}>
+      Lei aplicável
+    </Heading>
+
+    <Text>
+      Estes termos e condições são regidos e interpretados de acordo com as leis da SmartSpend e
+      você se submete irrevogavelmente à jurisdição exclusiva dos tribunais naquele estado ou
+      localidade.
+    </Text>
   </ScrollView>
 );
 
