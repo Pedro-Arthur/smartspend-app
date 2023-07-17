9a30bd1fee5592039cddd7c1fd1e0f07296791de
Pedro Arthur <pedro13arthur@gmail.com>
2023-07-16 21:51:07 -0300

    fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,48 +1,14 @@
-import React, { useContext } from 'react';
-import {
-  useColorModeValue,
-  Heading,
-  Center,
-  Button,
-  Switch,
-  useColorMode,
-  Text,
-  HStack,
-} from 'native-base';
-import { AuthContext } from '../contexts/AuthContext';
+import React from 'react';
+import { useColorModeValue, Heading, Center } from 'native-base';
 import TermsModal from '../components/TermsModal';
 
 const Home = () => {
   const bg = useColorModeValue('warmGray.50', 'coolGray.800');
-  const { colorMode, toggleColorMode } = useColorMode();
-  const { removeAuthIsLoggedIn, removeAuthUser, removeAuthToken } = useContext(AuthContext);
-
-  const logout = async (force) => {
-    if (force) {
-      await Promise.all([removeAuthIsLoggedIn(), removeAuthUser(), removeAuthToken()]);
-    } else {
-      await removeAuthIsLoggedIn();
-    }
-  };
 
   return (
     <>
       <Center flex="1" bg={bg} safeArea>
         <Heading>HOME</Heading>
-
-        <Text my={5}>Tema atual: {colorMode}</Text>
-
-        <HStack flexShrink={1} alignItems="center">
-          <Text>Modo escuro:</Text>
-          <Switch isChecked={colorMode === 'dark'} onToggle={toggleColorMode} mt={5} size="lg" />
-        </HStack>
-
-        <Button mt={5} onPress={() => logout(true)} w={40} backgroundColor="red.500">
-          force logout
-        </Button>
-        <Button mt={5} onPress={() => logout(false)} w={40} backgroundColor="red.500">
-          simulate expiration
-        </Button>
       </Center>
 
       <TermsModal />
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,22 @@
 import React, { useContext } from 'react';
-import { useColorModeValue, Heading, Center, Text, Button } from 'native-base';
+import {
+  useColorModeValue,
+  Heading,
+  Center,
+  Text,
+  Button,
+  useColorMode,
+  Switch,
+  HStack,
+} from 'native-base';
 import { AuthContext } from '../contexts/AuthContext';
 import api from '../services/api';
 
 const Profile = () => {
   const bg = useColorModeValue('warmGray.50', 'coolGray.800');
   const { user, token } = useContext(AuthContext);
+  const { removeAuthIsLoggedIn, removeAuthUser, removeAuthToken } = useContext(AuthContext);
+  const { colorMode, toggleColorMode } = useColorMode();
 
   const changeName = async () => {
     await api.patch(
@@ -19,13 +30,32 @@ const Profile = () => {
     );
   };
 
+  const logout = async (force) => {
+    if (force) {
+      await Promise.all([removeAuthIsLoggedIn(), removeAuthUser(), removeAuthToken()]);
+    } else {
+      await removeAuthIsLoggedIn();
+    }
+  };
+
   return (
     <Center flex="1" bg={bg} safeArea>
       <Heading>PROFILE</Heading>
       <Text>name: {user.name}</Text>
       <Text>final 4 chars token: {token.substring(token.length - 4)}</Text>
 
+      <Text my={5}>Tema atual: {colorMode}</Text>
+
+      <HStack flexShrink={1} alignItems="center">
+        <Text>Modo escuro:</Text>
+        <Switch isChecked={colorMode === 'dark'} onToggle={toggleColorMode} mt={5} size="lg" />
+      </HStack>
+
       <Button onPress={() => changeName()}>change name</Button>
+
+      <Button mt={5} onPress={() => logout(true)} w={40} backgroundColor="red.500">
+        force logout
+      </Button>
     </Center>
   );
 };
