a9294061f92e6a288d8f2324056aff91f1f67a33
Pedro Arthur <pedro13arthur@gmail.com>
2023-08-02 21:52:28 -0300

    fix env

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 import { API_BASE_URL } from '@env';
 
-const baseURL = API_BASE_URL;
-
 const api = axios.create({
-  baseURL,
+  baseURL: API_BASE_URL,
 });
 
 export default api;
