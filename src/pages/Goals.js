7c9f7ad21a4545c495dc0229182ad894e2c1a343
Pedro Arthur <pedro13arthur@gmail.com>
2023-08-12 17:30:40 -0300

    fix

--- src/pages/Goals.js
+++ src/pages/Goals.js
@@ -1,17 +1,31 @@
 import React, { useState } from 'react';
 import { useColorModeValue, Heading, Center, Text } from 'native-base';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import CurrencyInput from '../components/CurrencyInput';
 
 const Goals = () => {
   const bg = useColorModeValue('warmGray.100', 'dark.50');
 
   const [maxValue, setMaxValue] = useState(0);
+  const [date, setDate] = useState(new Date());
 
   return (
     <Center flex="1" bg={bg}>
       <Heading>GOALS</Heading>
       <Text my={4}>Valor: {maxValue}</Text>
       <CurrencyInput value={maxValue} onChangeText={(v) => setMaxValue(v)} maxDigits={9} />
+
+      <Text my={4}>Data: {date.toLocaleDateString()}</Text>
+      <DateTimePicker
+        maximumDate={new Date(2030, 1, 1)}
+        minimumDate={new Date()}
+        value={date}
+        onChange={(v) => console.log(v)}
+        display="default"
+        themeVariant={useColorModeValue('light', 'dark')}
+        mode="date"
+        textColor="#d97706"
+      />
     </Center>
   );
 };
