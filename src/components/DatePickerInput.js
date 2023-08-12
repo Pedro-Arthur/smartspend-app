30ce8d89c1b510a68120e0393c984cb4d58056cd
Pedro Arthur <pedro13arthur@gmail.com>
2023-08-12 19:56:59 -0300

    fix

--- src/components/DatePickerInput.js
+++ src/components/DatePickerInput.js
@@ -1,23 +1,33 @@
-import React, { useState } from 'react';
-import { Button, Platform } from 'react-native';
-import { useColorModeValue } from 'native-base';
+import React, { useState, useRef } from 'react';
+import { Platform } from 'react-native';
+import { useColorModeValue, Input, Icon } from 'native-base';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import { AntDesign } from '@expo/vector-icons';
 
 const DatePickerInput = ({ value, onChange, maximumDate, minimumDate }) => {
   const themeVariant = useColorModeValue('light', 'dark');
-
   const [showDatePicker, setShowDatePicker] = useState(false);
+  const inputRef = useRef(null);
 
   const handleDateChange = (event, selectedDate) => {
     if (event.type === 'set') {
       onChange(selectedDate);
     }
     setShowDatePicker(Platform.OS === 'ios');
+    inputRef.current.blur();
   };
 
   return (
     <>
-      <Button onPress={() => setShowDatePicker(true)} title="Selecione uma data" />
+      <Input
+        ref={inputRef}
+        InputLeftElement={
+          <Icon as={<AntDesign name="calendar" />} size={4} ml="3" color="muted.400" />
+        }
+        onFocus={() => setShowDatePicker(true)}
+        value={value.toLocaleDateString()}
+        showSoftInputOnFocus={false}
+      />
 
       {showDatePicker && (
         <DateTimePicker
