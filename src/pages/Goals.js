e2deee8fc65b57f234facc0513976a9d8ed28339
Pedro Arthur <pedro13arthur@gmail.com>
2023-08-12 18:09:13 -0300

    fix

--- src/pages/Goals.js
+++ src/pages/Goals.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
+import { Button, Platform } from 'react-native';
 import { useColorModeValue, Heading, Center, Text } from 'native-base';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import CurrencyInput from '../components/CurrencyInput';
 
 const Goals = () => {
   const bg = useColorModeValue('warmGray.100', 'dark.50');
+  const themeVariant = useColorModeValue('light', 'dark');
 
   const [maxValue, setMaxValue] = useState(0);
   const [date, setDate] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(false);
+
+  const handleDateChange = (event, selectedDate) => {
+    if (event.type === 'set') {
+      setDate(selectedDate);
+    }
+    setShowDatePicker(Platform.OS === 'ios');
+  };
 
   return (
     <Center flex="1" bg={bg}>
@@ -16,16 +26,20 @@ const Goals = () => {
       <CurrencyInput value={maxValue} onChangeText={(v) => setMaxValue(v)} maxDigits={9} />
 
       <Text my={4}>Data: {date.toLocaleDateString()}</Text>
-      <DateTimePicker
-        maximumDate={new Date(2030, 1, 1)}
-        minimumDate={new Date()}
-        value={date}
-        onChange={(v) => console.log(v)}
-        display="default"
-        themeVariant={useColorModeValue('light', 'dark')}
-        mode="date"
-        textColor="#d97706"
-      />
+
+      <Button onPress={() => setShowDatePicker(true)} title="Selecione uma data" />
+      {showDatePicker && (
+        <DateTimePicker
+          maximumDate={new Date(2030, 0, 1)}
+          minimumDate={new Date()}
+          value={date}
+          onChange={handleDateChange}
+          display="default"
+          themeVariant={themeVariant}
+          mode="date"
+          textColor="#d97706"
+        />
+      )}
     </Center>
   );
 };
