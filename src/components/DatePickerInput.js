3f742f0e9c72e573f033bc57623432ca06335b5d
Pedro Arthur <pedro13arthur@gmail.com>
2023-08-13 16:22:34 -0300

    fix

--- src/components/DatePickerInput.js
+++ src/components/DatePickerInput.js
@@ -2,21 +2,30 @@ import React, { useState, useRef } from 'react';
 import { Input, Icon, useColorModeValue } from 'native-base';
 import DatePicker from 'react-native-modern-datepicker';
 import { AntDesign } from '@expo/vector-icons';
-import { formatDate } from '../utils/helpers';
 
 const DatePickerInput = ({ value, onChange, maximumDate, minimumDate, current, placeholder }) => {
   const [showDatePicker, setShowDatePicker] = useState(false);
   const inputRef = useRef(null);
 
   const datePickerOptions = {
-    backgroundColor: useColorModeValue('#090C08', '#090C08'),
+    backgroundColor: useColorModeValue('#d6d3d1', '#090C08'),
     textHeaderColor: '#d97706',
     textDefaultColor: '#F6E7C1',
-    selectedTextColor: useColorModeValue('#fff', '#fff'),
+    selectedTextColor: useColorModeValue('#000', '#fff'),
     mainColor: '#d97706',
     textSecondaryColor: '#D6C7A1',
     borderColor: useColorModeValue('rgba(122, 146, 165, 0.1)', 'rgba(122, 146, 165, 0.1)'),
     defaultFont: 'montserrat-regular',
+  };
+
+  const onSelectedChange = (selectedValue) => {
+    const dateArray = selectedValue.split('/');
+    const year = parseInt(dateArray[0], 10);
+    const month = parseInt(dateArray[1], 10) - 1;
+    const day = parseInt(dateArray[2], 10);
+    onChange(new Date(year, month, day));
+    setShowDatePicker(false);
+    inputRef.current.blur();
   };
 
   return (
@@ -36,11 +45,11 @@ const DatePickerInput = ({ value, onChange, maximumDate, minimumDate, current, p
         <DatePicker
           options={datePickerOptions}
           current={current}
-          selected={formatDate(value)}
           mode="calendar"
           style={{ borderRadius: 8 }}
           minimumDate={minimumDate}
           maximumDate={maximumDate}
+          onSelectedChange={onSelectedChange}
         />
       )}
     </>
