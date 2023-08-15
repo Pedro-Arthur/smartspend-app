cd79412eaa30076a0473b8151c05bc82d8d76741
Pedro Arthur <pedro13arthur@gmail.com>
2023-08-14 22:08:49 -0300

    home button

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AntDesign } from '@expo/vector-icons';
-import { Text, useColorModeValue, Icon } from 'native-base';
+import { Text, useColorModeValue, Icon, Center } from 'native-base';
 import { Platform } from 'react-native';
 
 // Pages
@@ -12,6 +12,12 @@ import BankCards from '../pages/App/BankCards';
 import Goals from '../pages/App/Goals';
 
 const Tab = createBottomTabNavigator();
+
+const HomeButton = () => (
+  <Center bg="primary.600" borderRadius="full" w={12} h={12} mt="-6">
+    <Icon as={<AntDesign name="home" />} size={6} color="white" />
+  </Center>
+);
 
 const getTabOptions = (text, icon) => ({
   tabBarLabel: ({ color, focused }) => (
@@ -24,43 +30,58 @@ const getTabOptions = (text, icon) => ({
   ),
 });
 
-const AppRoutes = () => (
-  <Tab.Navigator
-    screenOptions={{
-      headerShown: false,
-      tabBarLabelStyle: {
-        fontFamily: 'montserrat-regular',
-      },
-      tabBarStyle: {
-        height: Platform.OS === 'android' ? 60 : 90,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.3,
-        shadowRadius: 4,
-      },
-      tabBarItemStyle: {
-        paddingVertical: 8,
-        backgroundColor: useColorModeValue('#fff', '#27272a'),
-      },
-      tabBarActiveTintColor: '#d97706',
-      tabBarInactiveTintColor: '#a1a1aa',
-    }}
-    initialRouteName="Home"
-  >
-    <Tab.Screen
-      name="BankAccounts"
-      component={BankAccounts}
-      options={getTabOptions('Contas', 'wallet')}
-    />
-    <Tab.Screen
-      name="BankCards"
-      component={BankCards}
-      options={getTabOptions('Cartões', 'creditcard')}
-    />
-    <Tab.Screen name="Home" component={Home} options={getTabOptions('Início', 'home')} />
-    <Tab.Screen name="Goals" component={Goals} options={getTabOptions('Metas', 'calendar')} />
-    <Tab.Screen name="Profile" component={Profile} options={getTabOptions('Perfil', 'user')} />
-  </Tab.Navigator>
-);
+const AppRoutes = () => {
+  const homeTabBarIcon = () => <HomeButton />;
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarLabelStyle: {
+          fontFamily: 'montserrat-regular',
+        },
+        tabBarStyle: {
+          height: Platform.OS === 'android' ? 60 : 90,
+          shadowColor: '#000',
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.3,
+          shadowRadius: 4,
+        },
+        tabBarItemStyle: {
+          paddingVertical: 8,
+          backgroundColor: useColorModeValue('#fff', '#27272a'),
+        },
+        tabBarActiveTintColor: '#d97706',
+        tabBarInactiveTintColor: '#a1a1aa',
+      }}
+      initialRouteName="Home"
+    >
+      <Tab.Screen
+        name="BankAccounts"
+        component={BankAccounts}
+        options={getTabOptions('Contas', 'wallet')}
+      />
+
+      <Tab.Screen
+        name="BankCards"
+        component={BankCards}
+        options={getTabOptions('Cartões', 'creditcard')}
+      />
+
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: homeTabBarIcon,
+        }}
+      />
+
+      <Tab.Screen name="Goals" component={Goals} options={getTabOptions('Metas', 'calendar')} />
+
+      <Tab.Screen name="Profile" component={Profile} options={getTabOptions('Perfil', 'user')} />
+    </Tab.Navigator>
+  );
+};
 
 export default AppRoutes;
