7217dc3dc5bf7b99270c3c723ed2d6a29a214e0b
Pedro Arthur <pedro13arthur@gmail.com>
2023-08-26 21:56:23 -0300

    fix

--- src/components/AddSpendFab.js
+++ src/components/AddSpendFab.js
@@ -13,87 +13,89 @@ const AddSpendFab = ({ openSaveModal }) => {
 
   return (
     <>
-      <Box position="absolute" right={5} bottom={63}>
-        <Stagger
-          visible={isOpen}
-          initial={{
-            opacity: 0,
-            scale: 0,
-            translateY: 34,
-          }}
-          animate={{
-            translateY: 0,
-            scale: 1,
-            opacity: 1,
-            transition: {
-              type: 'spring',
-              mass: 0.8,
-              stagger: {
-                offset: 30,
-                reverse: true,
+      {isOpen && (
+        <Box position="absolute" right={5} bottom={63}>
+          <Stagger
+            visible
+            initial={{
+              opacity: 0,
+              scale: 0,
+              translateY: 34,
+            }}
+            animate={{
+              translateY: 0,
+              scale: 1,
+              opacity: 1,
+              transition: {
+                type: 'spring',
+                mass: 0.8,
+                stagger: {
+                  offset: 30,
+                  reverse: true,
+                },
               },
-            },
-          }}
-          exit={{
-            translateY: 34,
-            scale: 0.5,
-            opacity: 0,
-            transition: {
-              duration: 100,
-              stagger: {
-                offset: 30,
-                reverse: true,
+            }}
+            exit={{
+              translateY: 34,
+              scale: 0.5,
+              opacity: 0,
+              transition: {
+                duration: 100,
+                stagger: {
+                  offset: 30,
+                  reverse: true,
+                },
               },
-            },
-          }}
-        >
-          <IconButton
-            mb="4"
-            variant="solid"
-            bg="primary.600"
-            colorScheme="primary"
-            borderRadius="full"
-            icon={<Icon as={MaterialCommunityIcons} color="white" size="6" name="bank-outline" />}
-            onPress={() => handlePress('transfer')}
-          />
-          <IconButton
-            mb="4"
-            variant="solid"
-            bg="primary.600"
-            colorScheme="primary"
-            borderRadius="full"
-            icon={<Icon as={<PixIcon width={24} height={24} fill="#fff" />} />}
-            onPress={() => handlePress('pix')}
-          />
-          <IconButton
-            mb="4"
-            variant="solid"
-            bg="primary.600"
-            colorScheme="primary"
-            borderRadius="full"
-            icon={<Icon as={AntDesign} color="white" size="6" name="creditcard" />}
-            onPress={() => handlePress('card')}
-          />
-          <IconButton
-            mb="4"
-            variant="solid"
-            bg="primary.600"
-            colorScheme="primary"
-            borderRadius="full"
-            icon={<Icon as={MaterialCommunityIcons} size="6" name="barcode-scan" color="white" />}
-            onPress={() => handlePress('ticket')}
-          />
-          <IconButton
-            mb="4"
-            variant="solid"
-            bg="primary.600"
-            colorScheme="primary"
-            borderRadius="full"
-            icon={<Icon as={MaterialIcons} size="6" name="attach-money" color="white" />}
-            onPress={() => handlePress('money')}
-          />
-        </Stagger>
-      </Box>
+            }}
+          >
+            <IconButton
+              mb="4"
+              variant="solid"
+              bg="primary.600"
+              colorScheme="primary"
+              borderRadius="full"
+              icon={<Icon as={MaterialCommunityIcons} color="white" size="6" name="bank-outline" />}
+              onPress={() => handlePress('transfer')}
+            />
+            <IconButton
+              mb="4"
+              variant="solid"
+              bg="primary.600"
+              colorScheme="primary"
+              borderRadius="full"
+              icon={<Icon as={<PixIcon width={24} height={24} fill="#fff" />} />}
+              onPress={() => handlePress('pix')}
+            />
+            <IconButton
+              mb="4"
+              variant="solid"
+              bg="primary.600"
+              colorScheme="primary"
+              borderRadius="full"
+              icon={<Icon as={AntDesign} color="white" size="6" name="creditcard" />}
+              onPress={() => handlePress('card')}
+            />
+            <IconButton
+              mb="4"
+              variant="solid"
+              bg="primary.600"
+              colorScheme="primary"
+              borderRadius="full"
+              icon={<Icon as={MaterialCommunityIcons} size="6" name="barcode-scan" color="white" />}
+              onPress={() => handlePress('ticket')}
+            />
+            <IconButton
+              mb="4"
+              variant="solid"
+              bg="primary.600"
+              colorScheme="primary"
+              borderRadius="full"
+              icon={<Icon as={MaterialIcons} size="6" name="attach-money" color="white" />}
+              onPress={() => handlePress('money')}
+            />
+          </Stagger>
+        </Box>
+      )}
 
       <Fab
         size="sm"
